f335a102882ad8773f4bd2bbb6326dbeeb2bcf7e
Amanda Ribeiro <amanda_ribeiro98@yahoo.com.br>
2023-03-19 21:18:18 -0300

    atualizando parte Java

---
+++ Conhecendo-a-Linguagem-de-Programção-Java/2- Ambiente de Desenvolvimento Java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author amand
+ *
+ */
+module aulaIDE {
+}
---
+++ Conhecendo-a-Linguagem-de-Programção-Java/3- aprendendo a sintaxe Java/anotomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,20 @@
+public class MinhaClasse {
+    
+public static void main (String [] args) {
+
+    String primeiroNome = "Amanda"
+    String segundoNome = "Blanglez"
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+    System.out.printIn(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome. String segundoNome) {
+    return "Resultado do método" + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
+
+
+
